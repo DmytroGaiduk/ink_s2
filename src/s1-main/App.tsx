9d5796c5444f752587c7385cb315d0e80137b2bf
Dmytro Gaiduk <dmitry.gaiduk@gmail.com>
2025-12-23 19:49:51 +0100

    test

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import s from './App.module.css'
-import HW9 from "../s2-homeworks/hw09/HW9";
+import HW5 from "../s2-homeworks/hw05/HW5";
+import HW11 from "../s2-homeworks/hw11/HW11";
 
 function App() {
     return (
         <div className={s.App}>
-            <HW9/>
+            <HW5/>
+            <HW11/>
         </div>
     )
 }
