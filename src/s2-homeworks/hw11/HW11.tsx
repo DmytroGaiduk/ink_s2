e843065e0a0602cb0224c11be50607234107b6c6
Dmytro Gaiduk <dmitry.gaiduk@gmail.com>
2025-12-29 13:45:59 +0100

    hw-11 playing

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,47 +1,53 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
+/*
+* 1 - передать значения в оба слайдера
+* 2 - дописать типы и логику функции change
+* 3 - сделать стили в соответствии с дизайном
+* */
+
 function HW11() {
+    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    // Универсальный обработчик, который подходит и для onChange, и для onChangeCommitted
-    const handleChange = (_event: any, newValue: number | number[]) => {
-        if (Array.isArray(newValue)) {
-            // double slider
-            setValue1(newValue[0])
-            setValue2(newValue[1])
+    const change = (event: any, value: any) => {
+
+        if (!Array.isArray(value) ) {
+            setValue1(value)
         } else {
-            // single slider
-            setValue1(newValue)
+            setValue1(value[0])
+            setValue2(value[1])
         }
+        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
     return (
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
+
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            onChange={change}
                             value={value1}
-                            onChange={handleChange}
-                            onChangeCommitted={handleChange}  // один и тот же обработчик
+                            // сделать так чтоб value1 изменялось // пишет студент
+
                         />
                     </div>
-
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            onChange={change}// сделать так чтоб value1/2 изменялось // пишет студент
                             value={[value1, value2]}
-                            onChange={handleChange}
-                            onChangeCommitted={handleChange}  // обязательно для Cypress!
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
